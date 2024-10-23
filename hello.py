9453632b6d06e1c823e2300d094ed91194945272
sanikaurade4 <147137518+sanikaurade4@users.noreply.github.com>
2024-10-23 09:30:03 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Calculate the square of a number
+def square(num):
+    return num**2
+
+# Test the function
+print(square(5))
+
+This text was aded originally while in the feature branch
+This text was added originally while in the feature branch
